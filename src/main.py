893499b4cf05c3d83e2dca245c6117ef9ca75b5f
vcBinary <v.caro@zaleos.net>
2025-03-29 17:04:40 +0100

    Added logger

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from view.Interface import Interface
 from controller.Controller import Controller
+import logging.config
 
 
 def main():  
-    Interface(Controller().generate_outputs).blocks.launch(debug=True)
+    logging.config.fileConfig("config/logging.conf")
+    controller = Controller()
+    Interface(controller.generate_outputs).blocks.launch(debug=True)
 
 if __name__ == "__main__":
     main()
