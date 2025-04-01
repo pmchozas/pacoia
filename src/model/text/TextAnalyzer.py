fc1765739592bfcae2e9ecc265354fa810b4b703
vcBinary <v.caro@zaleos.net>
2025-04-01 20:26:50 +0200

    Added more text metrics

--- src/model/text/TextAnalyzer.py
+++ src/model/text/TextAnalyzer.py
@@ -1,8 +1,9 @@
 from collections import Counter
+import textstat
 import re
 
 class TextAnalyzer:
-    
+
     @staticmethod
     def get_words(text: str) -> list[str]:
         return re.findall(r"\b\w+(?:[-']\w+)*\b", text)
@@ -17,3 +18,52 @@ class TextAnalyzer:
         frequencies = Counter(lower_case_words)
         return dict(sorted(frequencies.items(), key=lambda item: item[1], reverse=True))
     
+
+    @staticmethod
+    def get_flesch_reading_ease(text: str) -> float:
+        return textstat.flesch_reading_ease(text)
+    
+
+    @staticmethod
+    def get_flesch_kincaid_grade(text: str) -> float:
+        return textstat.flesch_kincaid_grade(text)
+    
+
+    @staticmethod
+    def get_smog_index(text: str) -> float:
+        return textstat.smog_index(text)
+    
+
+    @staticmethod
+    def get_coleman_liau_index(text: str) -> float:
+        return textstat.coleman_liau_index(text)
+    
+
+    @staticmethod
+    def get_automated_readability_index(text: str) -> float:
+        return textstat.automated_readability_index(text)
+    
+
+    @staticmethod
+    def get_dale_chall_readability_score(text: str) -> float:
+        return textstat.dale_chall_readability_score(text)
+    
+
+    @staticmethod
+    def get_difficult_words(text: str) -> int:
+        return textstat.difficult_words(text)
+    
+
+    @staticmethod
+    def get_linsear_write_formula(text: str) -> float:
+        return textstat.linsear_write_formula(text)
+    
+
+    @staticmethod
+    def get_gunning_fog(text: str) -> float:
+        return textstat.gunning_fog(text)
+    
+
+    @staticmethod
+    def get_text_standard(text: str) -> float:
+        return textstat.text_standard(text)
